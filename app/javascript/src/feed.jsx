920fe182710c4edfea3cf3ed576269f7fbd3fdac
jmferg11 <jfergie93@gmail.com>
2021-11-05 14:01:04 -0500

    add forms and text to home index

--- app/javascript/src/feed.jsx
+++ app/javascript/src/feed.jsx
@@ -8,18 +8,20 @@ const Feed = () => (
     <h1>Feed Page</h1>
       <div className='container'>
         <div className='navbar-header'>
-          <a class="navbar-brand" href="#">
-            <i class="fa fa-twitter"></i>
+          <a className="navbar-brand" href="#">
+            <i className="fa fa-twitter"></i>
           </a>
         </div>
         <ul className='nav navbar-right'>
           <li className='dropdown'>
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">language: <strong>English </strong><span class="caret"></span></a>
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">User</a>
             <ul class="dropdown-menu row" role="menu">
-              <li class="col-xs-12"><a href="#">Bahasa Malaya</a></li>
-              <li class="col-xs-12"><a href="#">Dansk</a></li>
-              <li class="col-xs-12"><a href="#">English</a></li>
-              <li class="col-xs-12"><a href="#">Suomi</a></li>
+              <li ><a href="#" class="username">User</a></li>
+              <li ><a href="#">Lists</a></li>
+              <li ><a href="#">Help</a></li>
+              <li ><a href="#">Keyboard shortcuts</a></li>
+              <li ><a href="#">Settings</a></li>
+              <li ><a id="log-out" href="#">Log out</a></li>
             </ul>
           </li>
         </ul>
@@ -30,12 +32,19 @@ const Feed = () => (
             <li className="nav-item">
               <a className="nav-link" href="/feed">Feed</a>
             </li>
-        </ul>
-        <div>
-          <span className="mr-3 text-secondary"><a href="https://github.com/Valatulkas" target="_blank" rel="noopener noreferrer">JFerg</a></span>
-        </div>
+        </ul> 
       </div>
     </nav>
+    <div className='main'>
+      <div className='container'>
+        <div className='row'>
+      
+        </div>
+      </div>
+    </div>
+    <div>
+      <span className="mr-3 text-secondary"><a href="https://github.com/Valatulkas" target="_blank" rel="noopener noreferrer">JFerg</a></span>
+    </div>
   </React.Fragment>
 )
 
--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -9,18 +9,18 @@ const Home = () => (
     <h1>Home Page</h1>
       <div className='container'>
         <div className='navbar-header'>
-          <a class="navbar-brand" href="#">
-            <i class="fa fa-twitter"></i>
+          <a className="navbar-brand" href="#">
+            <i className="fa fa-twitter"></i>
           </a>
         </div>
         <ul className='nav navbar-right'>
           <li className='dropdown'>
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">language: <strong>English </strong><span class="caret"></span></a>
-            <ul class="dropdown-menu row" role="menu">
-              <li class="col-xs-12"><a href="#">Bahasa Malaya</a></li>
-              <li class="col-xs-12"><a href="#">Dansk</a></li>
-              <li class="col-xs-12"><a href="#">English</a></li>
-              <li class="col-xs-12"><a href="#">Suomi</a></li>
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">language: <strong>English </strong><span class="caret"></span></a>
+            <ul className="dropdown-menu row" role="menu">
+              <li className="col-xs-12"><a href="#">Bahasa Malaya</a></li>
+              <li className="col-xs-12"><a href="#">Dansk</a></li>
+              <li className="col-xs-12"><a href="#">English</a></li>
+              <li className="col-xs-12"><a href="#">Suomi</a></li>
             </ul>
           </li>
         </ul>
@@ -32,11 +32,56 @@ const Home = () => (
               <a className="nav-link" href="/feed">Feed</a>
             </li>
         </ul>
+      </div>
+    </nav>
+    <div className='main'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6' id="welcome-text">
+            <h1><strong>Welcome to Twitter.</strong></h1>
+            <p>Connect with your friends &#8212; and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
+          </div>
+          <div className='col-md-6'>
+            <form>
+              <div className="form-group">
+                <input type="text" class="form-control username" placeholder="Username" />
+              </div>
+              <div className="form-group col-xs-8">
+                <input type="password" class="form-control password" placeholder="Password" />
+              </div>
+              <button id="log-in-btn" class="btn btn-default btn-primary col-xs-3 col-xs-offset-1">Log in</button>
+              <label>
+                <input type="checkbox" />
+                <span>Remember me</span>
+                <span> &#183; </span>
+              </label>
+              <a href="#">Forgot password?</a>
+            </form>
+          </div>
+          <div className='col-md-6'></div>
+          <div className='col-md-6'>
+            <form>
+              <div className="new-to-t">
+                <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
+              </div>
+              <div className="form-group">
+                <input type="text" class="form-control username" placeholder="Username" />
+              </div>
+              <div className="form-group">
+                <input type="email" class="form-control email" placeholder="Email" />
+              </div>
+              <div className="form-group">
+                <input type="password" class="form-control password" placeholder="Password" />
+              </div>
+              <button id="sign-up-btn" class="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
+            </form>
+          </div>
+        </div>
         <div>
           <span className="mr-3 text-secondary"><a href="https://github.com/Valatulkas" target="_blank" rel="noopener noreferrer">JFerg</a></span>
         </div>
       </div>
-    </nav>
+    </div>
   </React.Fragment>
 )
 
