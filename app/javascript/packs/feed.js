8c895cdfd9ac10c5651ad3c074866eb0d175cfac
jmferg11 <jfergie93@gmail.com>
2021-11-05 19:05:10 -0500

    add initial feed javascript magic

--- app/javascript/packs/feed.js
+++ app/javascript/packs/feed.js
@@ -1,3 +1,44 @@
-// feed.js
 import '@src/feed';
 
+$('.feeds.index').ready(function() {
+    var currentUser;
+
+    authenticate(function(response) {
+        console.log(response);
+        if(response.authenticated) {
+          currentUser = response.username;
+          $('#user-icon').text(currentUser);
+          $('.username').text(currentUser);
+          $('.screenName').text('@'+currentUser);
+          getUserTweets(currentUser, function(response) {
+            $('.user-stats-tweets').text(response.length);
+          });
+        } else {
+          window.location.replace("/");
+        }
+    }, function(error) {
+        console.log(error);
+    });
+    
+    function profileCard(username) {
+        $('.user-field .username').text(username);
+        $('.user-field .screenName').text('@'+username);
+        getUserTweets(username, function(response) {
+          $('.user-stats-tweets').text(response.length);
+        });
+    };
+    
+    $(document).on('click', '#log-out', function() {
+        logoutUser(function(){
+          authenticate(function(response) {
+            if(!response.authenticated) {
+              window.location.replace("/");
+            }
+          });
+        });
+    });
+
+    // Tweet Character Limit
+
+    
+})
